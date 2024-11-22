2f6928166b7065885277caf4e1c124fe4dc44f10
CarstenPaulo <pauloaugustocarsten@gmail.com>
2024-11-22 15:56:35 -0300

    first_commit

---
+++ website/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+
+def index(request):
+    return render(request, 'index.html')
+# def sobre(request):
+#     return render (request,'sobre.html')
+# def servicos(request):
+#     return render (request,'servicos.html')
+# def servicos(request):
+#     return render (request,'servicos.html')
